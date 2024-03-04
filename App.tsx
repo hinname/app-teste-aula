8ff439df71724ef45be3a293ca7be891aaddf083
hinname <herivelton.pro@gmail.com>
2024-03-04 09:27:38 -0300

    testing useState

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
       <Text>Hello World!!!</Text>
+      <View
+        style={{
+          borderBottomColor: 'black',
+          width: 100,
+          margin: 10,
+          borderBottomWidth: StyleSheet.hairlineWidth,
+        }}
+      />
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <Text>{count} clicks</Text>
       <StatusBar style="auto" />
     </View>
   );
