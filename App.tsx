9fe17304245090278cc1ee949ce6c75204c14ebe
hinname <herivelton.pro@gmail.com>
2024-03-25 10:05:50 -0300

    router begins

--- App.tsx
+++ App.tsx
@@ -1,98 +1,48 @@
 import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TextInput, Linking } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Login from './src/routes/Login';
+import SignUp from './src/routes/SignUp';
+import Notifications from './src/routes/Notifications';
+import Home from './src/routes/Home';
+import Profile from './src/routes/Profile';
+import Settings from './src/routes/Settings';
+import Contacts from './src/routes/Contacts';
 
 export default function App() {
-  const [loginDisabled, setLoginDisabled] = useState(true);
-  const [matricula, setMatricula] = useState('');
-  const [senha, setSenha] = useState('');
-
-  if (matricula.length > 0 && senha.length > 0 && loginDisabled !== false) {
-    setLoginDisabled(false);
-  }
-  if ((matricula.length === 0 || senha.length === 0) && loginDisabled !== true) {
-    setLoginDisabled(true);
-  }
+  const [isSignedin, setIsSignedin] = useState(false);
+  const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
+  const Stack2 = createStackNavigator();
+  const Drawer = createDrawerNavigator();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          style={{ width: 150, height: 50 }}
-          source={require('./assets/logo-estacio.png')}
-        />
-        <Text>SIA - Sistema de informações Acadêmicas</Text>
-        <Text style={{fontSize: 12}}>Segunda, 4 de Março de 2024</Text>
-      </View>
-      <View>
-        <Button title='Entrar com email de estudante'/>
-        <View style={styles.line}>
-          <View
-          style={{
-            borderBottomColor: 'black',
-            width: 125,
-            borderBottomWidth: StyleSheet.hairlineWidth,
-            }}
-          />
-          <Text>OU</Text>
-          <View
-          style={{
-            borderBottomColor: 'black',
-            width: 125,
-            borderBottomWidth: StyleSheet.hairlineWidth,
-            }}
-          />
-        </View>
-      </View>
-      <View style={styles.form}>
-        <Text>Matrícula</Text>
-        <TextInput style={styles.inputLogin} onChangeText={(value) => {setMatricula(value)}}/>
-        <Text style={styles.linkLogin} onPress={() => {Linking.openURL("https://www.google.com/")}}>Não sei ou esqueci a matrícula</Text>
-        <Text>Senha</Text>
-        <TextInput style={styles.inputLogin} onChangeText={(value) => {setSenha(value)}}/>
-        <Text style={styles.linkLogin} onPress={() => {Linking.openURL("https://estacio.br/")}}>Esqueci minha senha</Text>
-        <Button title='Entrar' disabled={loginDisabled}/>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {isSignedin ? (
+        <Drawer.Navigator>
+          <Drawer.Screen name="Main" component={
+            <Tab.Navigator>
+              <Tab.Screen name="Avisos" component={Notifications} />
+              <Tab.Screen name="Home" component={
+                <Stack.Navigator>
+                  <Stack.Screen name='Home_Stack' component={Home} />
+                  <Stack.Screen name='Perfil' component={Profile} />
+                </Stack.Navigator>
+              } />
+            </Tab.Navigator>
+          } />
+          <Drawer.Screen name='Config' component={Settings} />
+          <Drawer.Screen name='Contatos' component={Contacts} />
+        </Drawer.Navigator>
+        ): (
+          <Stack2.Navigator>
+            <Stack2.Screen name="Login" component={Login} />
+            <Stack2.Screen name="SignUp" component={SignUp} />
+          </Stack2.Navigator>
+        )}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  line: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 3,
-    marginTop: 10,
-  },
-  header: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  form: {
-    flexDirection: 'column',
-    alignContent: 'center',
-    justifyContent: 'center',
-    gap: 10,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    gap: 25,
-  },
-  linkLogin: {
-    textAlign: 'right', 
-    color: 'blue', 
-    fontSize: 10
-  },
-  inputLogin: {
-    padding: 10,
-    borderColor: 'black',
-    borderWidth: 0.5,
-    borderRadius: 5,
-  },
-});
---
+++ src/routes/Login.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Button, TextInput, Linking } from 'react-native';
+
+export default function Login() {
+  const [loginDisabled, setLoginDisabled] = useState(true);
+  const [matricula, setMatricula] = useState('');
+  const [senha, setSenha] = useState('');
+
+  if (matricula.length > 0 && senha.length > 0 && loginDisabled !== false) {
+    setLoginDisabled(false);
+  }
+  if ((matricula.length === 0 || senha.length === 0) && loginDisabled !== true) {
+    setLoginDisabled(true);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={{ width: 150, height: 50 }}
+          source={require('../../assets/logo-estacio.png')}
+        />
+        <Text>SIA - Sistema de informações Acadêmicas</Text>
+        <Text style={{fontSize: 12}}>Segunda, 4 de Março de 2024</Text>
+      </View>
+      <View>
+        <Button title='Entrar com email de estudante'/>
+        <View style={styles.line}>
+          <View
+          style={{
+            borderBottomColor: 'black',
+            width: 125,
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            }}
+          />
+          <Text>OU</Text>
+          <View
+          style={{
+            borderBottomColor: 'black',
+            width: 125,
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            }}
+          />
+        </View>
+      </View>
+      <View style={styles.form}>
+        <Text>Matrícula</Text>
+        <TextInput style={styles.inputLogin} onChangeText={(value) => {setMatricula(value)}}/>
+        <Text style={styles.linkLogin} onPress={() => {Linking.openURL("https://www.google.com/")}}>Não sei ou esqueci a matrícula</Text>
+        <Text>Senha</Text>
+        <TextInput secureTextEntry={true} style={styles.inputLogin} onChangeText={(value) => {setSenha(value)}}/>
+        <Text style={styles.linkLogin} onPress={() => {Linking.openURL("https://estacio.br/")}}>Esqueci minha senha</Text>
+        <Button title='Entrar' disabled={loginDisabled}/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  line: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 3,
+    marginTop: 10,
+  },
+  header: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    flexDirection: 'column',
+    alignContent: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    gap: 25,
+  },
+  linkLogin: {
+    textAlign: 'right', 
+    color: 'blue', 
+    fontSize: 10
+  },
+  inputLogin: {
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 0.5,
+    borderRadius: 5,
+  },
+});
